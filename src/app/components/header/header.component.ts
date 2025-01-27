a51aefc2f226b429865043c87a87766284a00fbe
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-01-27 21:35:53 +0530

    added more icon with cart functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyCartServiceService } from 'src/app/service/my-cart-service.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  cartItemCount: number = 0;
+  constructor(private readonly cartService: MyCartServiceService) {}
 
+  ngOnInit() {
+    this.cartService.cartItems$.subscribe((items: any[]) => {
+      this.cartItemCount = items.length;
+    });
+  }
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
+
+  openMyCard() {
+    this.cartService.openCart();
+  }
 }
