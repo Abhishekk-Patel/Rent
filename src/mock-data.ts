c862d0eaf8ca112348f88dbe8f634883470a1e4d
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-01-27 21:32:36 +0530

    added more raw data based on categories

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,76 +1,349 @@
 export const REWARD_LIST = [
-    {
-      pk: 987,
-      name: "Amazon",
-      points: 120,
-      display_img_url: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg",
-      quantity: 14,
-      valid_until: "2024-12-31T00:00:00",
-      low_quantity: 10,
-    },
-    {
-        pk: 987,
-        name: "Zara",
-        points: 100,
-        display_img_url: "https://ucarecdn.com/9a17572e-b060-44db-964b-31e7db76c0ff/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
-        quantity: 14,
-        valid_until: "2025-01-15T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Linkedin",
-        points: 50,
-        display_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1024px-LinkedIn_icon.svg.png",
-        quantity: 8,
-        valid_until: "2025-01-07T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Netflix",
-        points: 150,
-        display_img_url: "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
-        quantity: 0,
-        valid_until: "2024-12-31T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Hotstar",
-        points: 150,
-        display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnTXuRWfQ8pt_uT7Q1001vRxQc52T09lBkaQ&s",
-        quantity: 14,
-        valid_until: "2024-12-31T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Udemy",
-        points: 250,
-        display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJh04jt8o9Aer2DwrqC3pJV_hvoptcqYJoFg&s",
-        quantity: 2,
-        valid_until: "2024-12-31T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Zomato",
-        points: 50,
-        display_img_url: "https://upload.wikimedia.org/wikipedia/commons/7/75/Zomato_logo.png",
-        quantity: 14,
-        valid_until: "2024-12-31T00:00:00",
-        low_quantity: 10,
-      },
-      {
-        pk: 987,
-        name: "Swiggy",
-        points: 20,
-        display_img_url: "https://upload.wikimedia.org/wikipedia/commons/1/13/Swiggy_logo.png",
-        quantity: 15,
-        valid_until: "2024-12-31T00:00:00",
-        low_quantity: 10,
-      },
-   
-  ];
-  
+  // Ecommerce
+  {
+    pk: 987,
+    name: "Amazon",
+    points: 120,
+    display_img_url: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg",
+    quantity: 14,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "Fashion & Retail"
+  },
+  {
+    pk: 988,
+    name: "Zara",
+    points: 100,
+    display_img_url: "https://ucarecdn.com/9a17572e-b060-44db-964b-31e7db76c0ff/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+    quantity: 14,
+    valid_until: "2025-01-15T00:00:00",
+    low_quantity: 10,
+    category: "Fashion & Retail"
+  },
+  {
+    pk: 999,
+    name: "Best Buy",
+    points: 200,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWL51Brx3AuW2vhAdvfDmv-jR8EisPpjv4Pg&s",
+    quantity: 20,
+    valid_until: "2025-03-01T00:00:00",
+    low_quantity: 15,
+    category: "Fashion & Retail"
+  },
+  {
+    pk: 1000,
+    name: "Walmart",
+    points: 110,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9rUz60N_Oa1SQ-Fz4hcL_YCP6OgCSI-J8Cw&s",
+    quantity: 18,
+    valid_until: "2025-05-15T00:00:00",
+    low_quantity: 10,
+    category: "Fashion & Retail"
+  },
+  {
+    pk: 1001,
+    name: "eBay",
+    points: 130,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEA1mhKj4zG12q_U-IG-My-Xq5MNsy-0I0VQ&s",
+    quantity: 25,
+    valid_until: "2025-07-31T00:00:00",
+    low_quantity: 15,
+    category: "Fashion & Retail"
+  },
+
+  // Food Delivery
+  {
+    pk: 989,
+    name: "Zomato",
+    points: 50,
+    display_img_url: "https://upload.wikimedia.org/wikipedia/commons/7/75/Zomato_logo.png",
+    quantity: 14,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "food"
+  },
+  {
+    pk: 990,
+    name: "Swiggy",
+    points: 20,
+    display_img_url: "https://upload.wikimedia.org/wikipedia/commons/1/13/Swiggy_logo.png",
+    quantity: 15,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "food"
+  },
+  {
+    pk: 1002,
+    name: "Uber Eats",
+    points: 70,
+    display_img_url: "https://cdn.dribbble.com/users/1363333/screenshots/11389156/media/8ece61fb7ddd60c1b030295e42d45fcb.jpg?resize=400x300&vertical=center",
+    quantity: 10,
+    valid_until: "2025-01-31T00:00:00",
+    low_quantity: 5,
+    category: "food"
+  },
+  {
+    pk: 1003,
+    name: "DoorDash",
+    points: 80,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeK3AX1bvUyOm-ieomGB3Yd-UCwWumE_4oQ&s",
+    quantity: 12,
+    valid_until: "2025-02-28T00:00:00",
+    low_quantity: 7,
+    category: "food"
+  },
+  {
+    pk: 1004,
+    name: "Grubhub",
+    points: 60,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZL1MH33hVpLYi_j7iz9MGcxTXiNJA18VAXg&s",
+    quantity: 14,
+    valid_until: "2025-03-15T00:00:00",
+    low_quantity: 10,
+    category: "food"
+  },
+
+  // Entertainment
+  {
+    pk: 991,
+    name: "Netflix",
+    points: 150,
+    display_img_url: "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    quantity: 0,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "entertainment"
+  },
+  {
+    pk: 992,
+    name: "Hotstar",
+    points: 150,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnTXuRWfQ8pt_uT7Q1001vRxQc52T09lBkaQ&s",
+    quantity: 14,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "entertainment"
+  },
+  {
+    pk: 1005,
+    name: "Spotify",
+    points: 120,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF4i2h_y9d6x6SR8ozro3iSxCMrF48ibbBIw&s",
+    quantity: 10,
+    valid_until: "2025-03-15T00:00:00",
+    low_quantity: 5,
+    category: "entertainment"
+  },
+  {
+    pk: 1006,
+    name: "Hulu",
+    points: 130,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC-wXpLE307efBtZuVfi4zheygGQpuwRu7XA&s",
+    quantity: 20,
+    valid_until: "2025-06-30T00:00:00",
+    low_quantity: 15,
+    category: "entertainment"
+  },
+  {
+    pk: 1007,
+    name: "Disney+",
+    points: 140,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4NeIMAPIIBmzYarW_8efaVBAvEbFmeFxtrg&s",
+    quantity: 18,
+    valid_until: "2025-05-01T00:00:00",
+    low_quantity: 12,
+    category: "entertainment"
+  },
+
+  // Education
+  {
+    pk: 993,
+    name: "Udemy",
+    points: 250,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJh04jt8o9Aer2DwrqC3pJV_hvoptcqYJoFg&s",
+    quantity: 2,
+    valid_until: "2024-12-31T00:00:00",
+    low_quantity: 10,
+    category: "education"
+  },
+  {
+    pk: 1008,
+    name: "Coursera",
+    points: 200,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcLXiAUFRc5hMtcUuLk_bdIYZO3q_shTanAA&s",
+    quantity: 8,
+    valid_until: "2025-01-10T00:00:00",
+    low_quantity: 5,
+    category: "education"
+  },
+  {
+    pk: 1009,
+    name: "MasterClass",
+    points: 300,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRpelI_bW7cOUBI_paq4dkgAt_gObmssb0uQ&s",
+    quantity: 5,
+    valid_until: "2025-02-28T00:00:00",
+    low_quantity: 3,
+    category: "education"
+  },
+  {
+    pk: 1010,
+    name: "Skillshare",
+    points: 180,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1G2GeE6OChFlPeeKxZYd0k7RAqkCz8DBH0A&s",
+    quantity: 15,
+    valid_until: "2025-04-30T00:00:00",
+    low_quantity: 10,
+    category: "education"
+  },
+  {
+    pk: 1011,
+    name: "LinkedIn Learning",
+    points: 220,
+    display_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png",
+    quantity: 10,
+    valid_until: "2025-03-25T00:00:00",
+    low_quantity: 6,
+    category: "education"
+  },
+
+  // Sports
+  {
+    pk: 994,
+    name: "Nike Store",
+    points: 200,
+    display_img_url: "https://res.cloudinary.com/dmubfrefi/image/private/s--X0LLoOBX--/c_crop,h_2728,w_4090,x_334,y_0/f_auto/q_auto/v1/dee-about-cms-prod-medias/cf68f541-fc92-4373-91cb-086ae0fe2f88/002-nike-logos-swoosh-white.jpg?_a=BAAAV6Bs",
+    quantity: 10,
+    valid_until: "2025-02-28T00:00:00",
+    low_quantity: 5,
+    category: "sports"
+  },
+  {
+    pk: 995,
+    name: "Adidas",
+    points: 180,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIbTAS4SeBn3fgSQNLRzKL1Ci77-ZBnuEilA&s",
+    quantity: 8,
+    valid_until: "2025-01-30T00:00:00",
+    low_quantity: 5,
+    category: "sports"
+  },
+  {
+    pk: 1004,
+    name: "Reebok",
+    points: 150,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj6TSlyhn_DKpAM-Gs5xerEYNOMHIRMVq-hg&s",
+    quantity: 12,
+    valid_until: "2025-03-01T00:00:00",
+    low_quantity: 6,
+    category: "sports"
+  },
+  {
+    pk: 1012,
+    name: "Puma",
+    points: 170,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmEw8TrscvmCGKcZO9Z9g0sfzQv2gHyWvz2g&s",
+    quantity: 11,
+    valid_until: "2025-05-15T00:00:00",
+    low_quantity: 8,
+    category: "sports"
+  },
+  {
+    pk: 1013,
+    name: "Under Armour",
+    points: 160,
+    display_img_url: "https://i0.wp.com/www.sportswearcode.com/wp-content/uploads/2020/08/UA-Logo.gif?fit=576%2C401&ssl=1",
+    quantity: 10,
+    valid_until: "2025-04-30T00:00:00",
+    low_quantity: 6,
+    category: "sports"
+  },
+
+  // Travel
+  {
+    pk: 1014,
+    name: "Airbnb",
+    points: 500,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0WQ2G_4bhRrG8UKG5ru3xhZDzlGox9q8GmA&s",
+    quantity: 6,
+    valid_until: "2025-12-31T00:00:00",
+    low_quantity: 3,
+    category: "travel"
+  },
+  {
+    pk: 1015,
+    name: "Booking.com",
+    points: 400,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfssr3WShudJkxjmst-dbjxF_OLmhNdv6Fxg&s",
+    quantity: 8,
+    valid_until: "2025-11-15T00:00:00",
+    low_quantity: 4,
+    category: "travel"
+  },
+  {
+    pk: 1016,
+    name: "Expedia",
+    points: 450,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGExGUvkyhM5OibqMumh7SIvOsSmR8aObThw&s",
+    quantity: 7,
+    valid_until: "2025-08-30T00:00:00",
+    low_quantity: 4,
+    category: "travel"
+  },
+
+  // Health & Wellness
+  {
+    pk: 1017,
+    name: "GNC",
+    points: 300,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWegl1li0BqQeY_EVH4jFrMciBYF9-vlporw&s",
+    quantity: 10,
+    valid_until: "2025-06-30T00:00:00",
+    low_quantity: 5,
+    category: "Health & Wellness"
+  },
+  {
+    pk: 1018,
+    name: "MyFitnessPal",
+    points: 350,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVid_mRsjF099OzB_Eb1uKBswebs6IDRvLRw&s",
+    quantity: 15,
+    valid_until: "2025-04-15T00:00:00",
+    low_quantity: 10,
+    category: "Health & Wellness"
+  },
+
+  // Technology
+  {
+    pk: 1005,
+    name: "Apple Store",
+    points: 500,
+    display_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGP-40HJCeCvfaQUnNJLy6aOjdK6_M8v6VqQ&s",
+    quantity: 5,
+    valid_until: "2025-06-30T00:00:00",
+    low_quantity: 3,
+    category: "technology"
+  },
+  {
+    pk: 1006,
+    name: "Samsung",
+    points: 400,
+    display_img_url: "https://images.samsung.com/is/image/samsung/assets/global/about-us/brand/logo/256_144_4.png?$512_N_PNG$",
+    quantity: 7,
+    valid_until: "2025-04-15T00:00:00",
+    low_quantity: 4,
+    category: "technology"
+  }
+];
+
+export const Category_LIST = [
+  { name: 'All', isExpanded: true, isSelected: true },
+  { name: 'Entertainment', isExpanded: false, isSelected: false },
+  { name: 'Sports', isExpanded: false, isSelected: false },
+  { name: 'Education', isExpanded: false, isSelected: false },
+  { name: 'Fashion & Retail', isExpanded: false, isSelected: false },
+  { name: 'Travel', isExpanded: false, isSelected: false },
+  { name: 'Technology', isExpanded: false, isSelected: false },
+  { name: 'Health & Wellness', isExpanded: false, isSelected: false },
+  { name: 'Food', isExpanded: false, isSelected: false },
+];
