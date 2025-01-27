e739ee1612a585bb7b284c54a3bed42110da36da
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-01-27 21:33:22 +0530

    added routing for mycart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SortPanelComponent } from './components/sort-panel/sort-panel.component';
+import { MyCartComponent } from './components/my-cart/my-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sort-panel', component: SortPanelComponent },
+  { path: 'myCart', component: MyCartComponent } // Ensure this path matches the one used in openCart
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
