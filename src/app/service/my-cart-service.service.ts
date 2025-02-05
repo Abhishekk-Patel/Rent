94f9fd012233b92e60010e930bb6834770118cbb
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-02-05 22:40:41 +0530

    clear console and updated some logic to add cart

--- src/app/service/my-cart-service.service.ts
+++ src/app/service/my-cart-service.service.ts
@@ -1,36 +1,43 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MyCartComponent } from '../components/my-cart/my-cart.component'; // Adjust the path as necessary
+import { MatDialog } from '@angular/material/dialog';
+import { MyCartComponent } from '../components/my-cart/my-cart.component';
 import { ProductDetailsPopupComponent } from '../components/product-details-popup/product-details-popup.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
- import { signal } from '@angular/core';
-
+import { signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyCartServiceService {
-
   private readonly cartItems = new BehaviorSubject<any[]>([]);
   cartItems$ = this.cartItems.asObservable();
-  isUser:string = '';
- myCartValue = signal(0);
-  constructor(private readonly router: Router, private readonly dialog: MatDialog,private readonly snackBar: MatSnackBar) {
-   }
- 
+  isUser: string = '';
+  myCartValue = signal(0);
+  public isAddNewProductSubject = new BehaviorSubject<boolean>(false);
+  isAddNewProduct$ = this.isAddNewProductSubject.asObservable();
+
+  constructor(
+    private readonly router: Router,
+    private readonly dialog: MatDialog,
+    private readonly snackBar: MatSnackBar
+  ) {}
+
+  setIsAddNewProduct(value: boolean): void {
+    this.isAddNewProductSubject.next(value);
+  }
+
   addToCart(item: any): void {
     const currentItems = this.cartItems.value;
-    const itemExists = currentItems.some(cartItem => cartItem == item ); // Check for duplicate
-    this.showMessage('Item already in cart'); // Show message if item exists
+    const itemExists = currentItems.some((cartItem) => cartItem == item);
 
-   if (!itemExists) {
+    if (!itemExists) {
       this.cartItems.next([...currentItems, item]);
-      this.myCartValue.update(value => value + 1); // Update cart value
-      this.showMessage('Product successfully added in cart'); // Show message if item is added
+      this.myCartValue.update((value) => value + 1);
+      this.showMessage('Product successfully added in cart');
     } else {
-      this.showMessage('Item already in cart'); // Show message if item exists
+      this.showMessage('Item already in cart');
     }
   }
 
@@ -40,35 +47,33 @@ export class MyCartServiceService {
 
   removeFromCart(item: any): void {
     const currentItems = this.cartItems.value;
-    const updatedItems = currentItems.filter(cartItem => cartItem !== item.pk);
+    const updatedItems = currentItems.filter((cartItem) => cartItem !== item.pk);
 
     if (updatedItems.length !== currentItems.length) {
       this.cartItems.next(updatedItems);
-      this.myCartValue.update(value => value - 1); // Update cart value
+      this.myCartValue.update((value) => value - 1);
     } else {
-      this.showMessage('Item not found in cart'); // Show message if item not found
-      
+      this.showMessage('Item not found in cart');
     }
   }
 
   openCart(): void {
-   
-      this.dialog.open(MyCartComponent);
-    
+    this.dialog.open(MyCartComponent);
   }
+
   openProductDetails(primaryKey: number): void {
     this.dialog.open(ProductDetailsPopupComponent, {
-      data: { primaryKey }
+      data: { primaryKey },
     });
-}
+  }
 
   itemExistsInCart(pk: number): boolean {
-    return this.cartItems.value.some(cartItem => cartItem === pk);
+    return this.cartItems.value.some((cartItem) => cartItem === pk);
   }
 
   showMessage(message: string): void {
     this.snackBar.open(message, 'Close', {
-      duration: 3000, // Duration in milliseconds
+      duration: 3000,
     });
   }
 
@@ -85,4 +90,3 @@ export class MyCartServiceService {
     return this.myCartValue();
   }
 }
-
