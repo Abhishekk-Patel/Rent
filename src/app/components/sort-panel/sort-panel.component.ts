4f3f2f233dbabffc9c74c5f711863da222189ab5
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-02-06 21:20:43 +0530

    add filter by price and location

--- src/app/components/sort-panel/sort-panel.component.ts
+++ src/app/components/sort-panel/sort-panel.component.ts
@@ -8,10 +8,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class SortPanelComponent {
   @Output() close = new EventEmitter<void>();
   @Output() sort = new EventEmitter<string>();
+  @Output() filterByPrice = new EventEmitter<{ min: number, max: number }>();
+  @Output() filterByLocation = new EventEmitter<string>();
 
   isAscApplied: boolean = false;
   isDescApplied: boolean = false;
   selectedSort: string | null = null;
+  minPrice: number | null = null;
+  maxPrice: number | null = null;
+  location: string | null = null;
 
   applySort(order: string): void {
     if (order === 'asc') {
@@ -26,15 +31,26 @@ export class SortPanelComponent {
     this.selectedSort = null;
     this.isAscApplied = false;
     this.isDescApplied = false;
+    this.minPrice = null;
+    this.maxPrice = null;
+    this.location = null;
   }
 
   apply(): void {
     if (this.selectedSort) {
       this.sort.emit(this.selectedSort);
-      this.close.emit();
-      this.selectedSort = null;
     }
+    if (this.isPriceFilterApplied()) {
+      this.filterByPrice.emit({ min: this.minPrice!, max: this.maxPrice! });
+    }
+    if (this.location) {
+      this.filterByLocation.emit(this.location);
+    }
+    this.close.emit();
+    this.selectedSort = null;
   }
 
-
+  isPriceFilterApplied(): boolean {
+    return this.minPrice !== null || this.maxPrice !== null;
+  }
 }
