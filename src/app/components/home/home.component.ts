7d5f0fb7fb7e7bbb06eeded84e1e93fa73f0d9f3
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-02-07 22:37:23 +0530

    added login and sign-in feature

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MyCartServiceService } from 'src/app/service/my-cart-service.service';
 
 @Component({
@@ -7,14 +8,36 @@ import { MyCartServiceService } from 'src/app/service/my-cart-service.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   showRentOptions: boolean = false;
-  constructor(public router: Router, public myCartSercvice: MyCartServiceService) {
+  showLogin: boolean = false;
+  showSignUp: boolean = false;
+  loginForm!: FormGroup;
+  signUpForm!: FormGroup;
+
+  constructor(
+    public router: Router,
+    public myCartSercvice: MyCartServiceService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.signUpForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobileNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
   }
 
   navigateTo(user: string) {
     this.myCartSercvice.setUser(user);
-   
     this.router.navigate(['/content']);
   }
 
@@ -28,5 +51,31 @@ export class HomeComponent {
 
   goBack() {
     this.showRentOptions = false;
+  }
+
+  toggleLogin() {
+    this.showLogin = !this.showLogin;
+    this.showSignUp = false;
+  }
+
+  toggleSignUp() {
+    this.showSignUp = !this.showSignUp;
+    this.showLogin = false;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      // Implement login logic here
+      console.log(this.loginForm.value);
+      this.toggleLogin();
+    }
+  }
+
+  signUp() {
+    if (this.signUpForm.valid) {
+      // Implement sign-up logic here
+      console.log(this.signUpForm.value);
+      this.toggleSignUp();
+    }
   }
 }
