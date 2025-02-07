cec500e7e7ffec3ba22b99c83c78b98b04f7621f
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-02-07 19:06:24 +0530

    removed heaer and footer from content and used globally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SortPanelComponent } from './components/sort-panel/sort-panel.component';
-import { MyCartComponent } from './components/my-cart/my-cart.component';
-import { ProductDetailsPopupComponent } from './components/product-details-popup/product-details-popup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContentComponentComponent } from './components/content-component/content-component.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Set HomeComponent as the default route
-  { path: 'content', component: ContentComponentComponent }, 
-  { path: 'header', component: HeaderComponent}, 
-  { path: 'footer', component: FooterComponent}, 
-  { path: 'sort-panel', component: SortPanelComponent },
-  { path: 'myCart', component: MyCartComponent }, // Ensure this path matches the one used in openCart
-  { path: 'productDetails', component: ProductDetailsPopupComponent }, // product details component will call on click know more btn
+  { path: 'content', component: ContentComponentComponent },
   { path: 'add-product', component: AddProductComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect any unknown paths to HomeComponent
-
 ];
 
 @NgModule({
