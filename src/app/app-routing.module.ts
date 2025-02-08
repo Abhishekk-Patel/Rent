a5a0b93502e6b93b432e305c93ddcc6ffd4294e8
abhishek.patel <abhishek.patel@wolterskluwer.com>
2025-02-08 13:09:20 +0530

    created new myacoount comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContentComponentComponent } from './components/content-component/content-component.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Set HomeComponent as the default route
   { path: 'content', component: ContentComponentComponent },
   { path: 'add-product', component: AddProductComponent },
+  { path: 'my-account', component: MyAccountComponent }, 
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect any unknown paths to HomeComponent
 ];
 
