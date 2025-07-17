79087d1b2947c642d6c6e9e1b2d56ec49b0d8c95
abhishek patel <abhishek.patel@wolterskluwer.com>
2025-07-17 23:21:21 +0530

    updated authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,8 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'content', component: ContentComponentComponent },
-  { path: 'add-product', component: AddProductComponent, },
-  { path: 'my-account', component: MyAccountComponent,  }, 
+  { path: 'add-product', component: AddProductComponent, canActivate: [AuthGuard] },
+  { path: 'my-account', component: MyAccountComponent, canActivate: [AuthGuard] }, 
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect any unknown paths to HomeComponent
 ];
 
