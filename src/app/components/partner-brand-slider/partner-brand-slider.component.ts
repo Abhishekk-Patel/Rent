0ae45f5878ce53efcde715446161e47fdb375b38
Ap15121997@ <abhishekpate687@gmail.com>
2025-11-15 02:12:53 +0530

    update design for mobile

--- src/app/components/partner-brand-slider/partner-brand-slider.component.ts
+++ src/app/components/partner-brand-slider/partner-brand-slider.component.ts
@@ -8,6 +8,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   styleUrls: ['./partner-brand-slider.component.css'],
 })
 export class PartnerBrandSliderComponent {
+
+
   scrollToCatalog(): void {
     setTimeout(() => {
       const catalogEl = document.querySelector('.catalog');
@@ -20,16 +22,16 @@ export class PartnerBrandSliderComponent {
     // { name: 'Brand 3', imageUrl: './assets/22.webp' },
     // { name: 'Brand 2', imageUrl: './assets/16.png' },
     // { name: 'Brand 1', imageUrl: './assets/20.webp' },
-    
+
     // { name: 'Brand 4', imageUrl: './assets/23.webp' },
     // { name: 'Brand 5', imageUrl: './assets/24.webp' },
     // { name: 'Brand 6', imageUrl: './assets/25.webp' },
     // { name: 'Brand 7', imageUrl: './assets/26.webp' },
     // { name: 'Brand 8', imageUrl: './assets/27.webp' },
     // { name: 'Brand 9', imageUrl: './assets/28.webp' },
-   
-    //  {name:'Brannd10', imageUrl: './assets/CTALow2.png'},
-  {name:'Brannd10', imageUrl: './assets/CTA22.jpg'},
+
+  {name:'Brannd10', imageUrl: './assets/CTALow2.png'},
+  //{name:'Brannd10', imageUrl: './assets/CTA22.jpg'},
 
 
   ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
- apiBaseUrl: 'http://localhost:3000',
- //apiBaseUrl: 'https://rent-be.onrender.com',
-socketUrl: 'http://localhost:4000'
-//socketUrl: 'https://rent-be.onrender.com',
+  production: true,
+ //apiBaseUrl: 'http://localhost:3000',
+ apiBaseUrl: 'https://rent-be.onrender.com',
+//socketUrl: 'http://localhost:4000'
+socketUrl: 'https://rent-be.onrender.com',
 };
