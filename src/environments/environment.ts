18e0ac04bac172df0a4ca5f3eef1f77c81dfb4db
Abhishek L Patel <abhishek.l.patel@pwc.com>
2026-01-24 22:53:24 +0530

    new build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //apiBaseUrl: 'http://localhost:3000',
   apiBaseUrl: 'https://rent-be.onrender.com',
-  //socketUrl: 'http://localhost:4000'
+  //socketUrl: 'http://localhost:3000'
   socketUrl: 'https://rent-be.onrender.com',
 };
